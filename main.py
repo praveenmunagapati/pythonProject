ed0822cc13a487d28e12335bb69b355c0bed3f99
lab1 <piratepieinc@gmail.com>
2023-12-23 10:56:52 +0530

    wrote fabinacci program

--- main.py
+++ main.py
@@ -1,4 +1,50 @@
-#usr/bin/python
+a = 0
+b = 1
+c = a+b
 
-#prints hello world!
-print("Hello World!")
+#1 interation
+print(a)
+print(b)
+print(c)
+
+#2 interation
+a = b
+b = c
+c= a+b
+print(c)
+#3 interation
+
+a = b
+b = c
+c= a+b
+print(c)
+#4 interation
+
+a = b
+b = c
+c= a+b
+print(c)
+#5 interation
+
+a = b
+b = c
+c= a+b
+print(c)
+#6 interation
+
+a = b
+b = c
+c= a+b
+print(c)
+
+limit = input("enter number of fab to display  : ")
+limit = int(limit)
+print(limit)
+print(type(limit))
+
+while limit > 0:
+    a = b
+    b = c
+    c = a + b
+    print(c)
+    limit = limit - 1
