4a885f547f3eac51f1595e57b6488a38057650fd
lab1 <piratepieinc@gmail.com>
2024-02-19 15:10:08 +0530

    inheritances

--- oops/classandobject.py
+++ oops/classandobject.py
@@ -1,101 +1,63 @@
-# """
-#     concept based on real world entities
-#
-#     can make your own type
-#
-#     code reusability
-#
-#
-#     syntax
-#
-#     class class_name :
-#         data members
-#         functions
-#
-# """
-#
-# class human:
-#     pass
-#
-# #object syntax
-# man = human()
-#
-# print(man)
-# print(type(man))
-#
-# class human:
-#     def __init__(self):
-#         print(" i am a  human")
-#
-# man = human()
-#
-# class human:
-#     def __init__(human):
-#         print(" i am a  human")
-#
-# man = human()
-#
-#
-# class human:
-#     spiecies = None
-#     def __init__(self,type):
-#         print(" i am a  human")
-#         self.spiecies = type
-#     def printme(self):
-#         print(self.spiecies)
-#
-# man = human("home sapians")
-# man.printme()
-#
-# class human:
-#     def __init__(self,type):
-#         print(" i am a  human")
-#         self.spiecies = type
-#     def printme(self):
-#         print(self.spiecies)
-#
-# man = human("home sapians")
-# man.printme()
-#
+"""
+    concept based on real world entities
 
-# inheritance
+    can make your own type
 
-class base:
-    somevar = 10
+    code reusability
+
+
+    syntax
+
+    class class_name :
+        data members
+        functions
+
+"""
+
+class human:
+    pass
+
+#object syntax
+man = human()
+
+print(man)
+print(type(man))
+
+class human:
     def __init__(self):
-        print(" i am a  base")
+        print(" i am a  human")
+
+man = human()
+
+class human:
+    def __init__(human):
+        print(" i am a  human")
+
+man = human()
 
 
-class derived (base):
-    def __init__(self):
-        print(" i am a  derived")
-        self.anothervar = self.somevar
-
+class human:
+    spiecies = None
+    def __init__(self,type):
+        print(" i am a  human")
+        self.spiecies = type
     def printme(self):
-        print(self.somevar)
-        print(self.anothervar)
+        print(self.spiecies)
 
-dev = derived()
-dev.printme()
+man = human("home sapians")
+man.printme()
 
-
-
-
-
-class base:
-    somevar = 10
-    def __init__(self):
-        print(" i am a  base")
-
-
-class derived (base):
-    def __init__(self):
-        print(" i am a  derived")
-        self.anothervar = self.somevar
-
+class human:
+    def __init__(self,type):
+        print(" i am a  human")
+        self.spiecies = type
     def printme(self):
-        print(self.somevar)
-        print(self.anothervar)
+        print(self.spiecies)
 
-dev = derived()
-dev.printme()
+man = human("home sapians")
+man.printme()
+
+
+
+
+
---
+++ oops/inheritance.py
@@ -0,0 +1,137 @@
+#
+# # inheritance
+#
+# class base:
+#     somevar = 10
+#     def __init__(self):
+#         print(" i am a  base")
+#
+#
+# class derived (base):
+#     def __init__(self):
+#         print(" i am a  derived")
+#         self.anothervar = self.somevar
+#
+#     def printme(self):
+#         print(self.somevar)
+#         print(self.anothervar)
+#
+# dev = derived()
+# dev.printme()
+#
+#
+#
+# class me:
+#     mymoney = 10
+#     def __init__(self):
+#         print(" i am me")
+#
+#
+# class myfriend (me):
+#     def __init__(self):
+#         print(" i am a  myfriend")
+#         self.myfriendmoney = self.mymoney
+#
+#     def printme(self):
+#         print(self.mymoney)
+#
+# class myfriendfriend (myfriend):
+#     def __init__(self):
+#         print(" i am a  myfriendfriend")
+#         self.myfriendfriendmoney = self.mymoney
+#
+#     def printme(self):
+#         print(self.mymoney)
+#
+# myfriendfriendobject = myfriendfriend()
+# myfriendfriendobject.printme()
+#
+#
+#
+# class A:
+#     class_a_var = 15
+#     def __init__(self):
+#         print("i am A")
+#
+#
+# class B:
+#     class_b_var = 20
+#     def __init__(self):
+#         print("i am B")
+#
+#
+# class C (A,B):
+#     def __init__(self):
+#         print("i am C")
+#         self.total = self.class_a_var + self.class_b_var
+#
+#     def printtotal(self):
+#         print('totatl is ', self.total)
+#
+#
+# cobject = C()
+# cobject.printtotal()
+#
+#
+# class Total:
+#     def __init__(self):
+#         self.total = 0
+# class Add(Total):
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+#         self.total = self.a + self.b
+#     def printtotal(self):
+#         print('add is ' ,self.total)
+#
+# class Mul(Total):
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+#         self.total = self.a * self.b
+#     def printtotal(self):
+#         print('mul is ' ,self.total)
+#
+# class Div(Total):
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+#         self.total = self.a / self.b
+#     def printtotal(self):
+#         print('div is ' ,self.total)
+#
+#
+#
+# add = Add(5,6)
+# add.printtotal()
+# mul = Mul(5,6)
+# mul.printtotal()
+# div = Div(5,6)
+# div.printtotal()
+
+
+class Numbers:
+    def __init__(self):
+        self.alist = [1,2,3,4,5,6,7,8,9]
+
+class Sumofelements(Numbers):
+    def __init__(self):
+        super().__init__()
+        self.total  = sum(self.alist)
+
+class Noofelements(Numbers):
+    def __init__(self):
+        super().__init__()
+        self.nolements = len(self.alist)
+
+class Mean(Sumofelements,Noofelements):
+    def __init__(self):
+        super().__init__()
+        self.mean = self.total / self.nolements
+
+    def printmean(self):
+        print(self.mean)
+meanofalist = Mean()
+meanofalist.printmean()
+
+#https://chat.openai.com/share/72925310-29fa-4c7b-a9e2-e0a2698c46c6
